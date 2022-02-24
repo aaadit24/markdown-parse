ef7a7a06f822168ea476cb7cbc3844bf5524fd00
Aadit Agarwal <cs15lwi22aeg@ieng6-203.ucsd.edu>
2022-02-24 10:37:31 -0800

    h

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,21 +11,28 @@ public class MarkdownParse {
         // the next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            int nextOpenBracket = markdown.indexOf("[", currentIndex),
+                nextCloseBracket = markdown.indexOf("]", nextOpenBracket),
+                openParen = markdown.indexOf("(", nextCloseBracket),
+                closeParen = markdown.indexOf(")", openParen);
 
-            // if(nextOpenBracket >= 0 && nextCloseBracket >= 0 && openParen == nextCloseBracket+1 && closeParen >= 0){
-            //     toReturn.add(markdown.substring(openParen + 1, closeParen));
-            //     currentIndex = closeParen + 1;
-            // }else{
-            //     currentIndex += 1;
-            // }
-
-
+            if(nextOpenBracket > 0 &&
+               markdown.charAt(nextOpenBracket-1) == '!'){
+                // Image, skip
+                currentIndex = nextOpenBracket + 1;
+            } else if(nextOpenBracket >= 0 &&
+                      nextCloseBracket >= 0 &&
+                      nextCloseBracket > nextOpenBracket+1 &&
+                      openParen == nextCloseBracket+1 &&
+                      closeParen >= 0
+            ){
+                // Valid link, add to list
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+            } else {
+                // Invalid link, advance one character
+                currentIndex += 1;
+            }
         }
         return toReturn;
     }
@@ -34,6 +41,5 @@ public class MarkdownParse {
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
-        
     }
 }
